515f77000264500ec8af0d84cc9184ef335ebb9e
Swati Rasal <swatirasal2001@gmail.com>
2022-02-25 13:26:05 +0530

    Updates

---
+++ precache-manifest.6c6b65234f7d59818f89bc25d2edb694.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "398df4b8852e6fd8ce87",
+    "url": "/todolistwebsite/static/css/main.aa5a09a4.chunk.css"
+  },
+  {
+    "revision": "398df4b8852e6fd8ce87",
+    "url": "/todolistwebsite/static/js/main.3779e787.chunk.js"
+  },
+  {
+    "revision": "b40a995e03efb383f505",
+    "url": "/todolistwebsite/static/js/runtime~main.fa979334.js"
+  },
+  {
+    "revision": "e6f956e6731dc07de0fe",
+    "url": "/todolistwebsite/static/js/2.dd4c5486.chunk.js"
+  },
+  {
+    "revision": "9c19088db84563606874",
+    "url": "/todolistwebsite/static/js/3.5526bd81.chunk.js"
+  },
+  {
+    "revision": "aca2d3641aa4afb6b9c6ba0a31fe71d5",
+    "url": "/todolistwebsite/static/media/image5.aca2d364.jpg"
+  },
+  {
+    "revision": "13341fbb4fba36f664026702806dbeb8",
+    "url": "/todolistwebsite/index.html"
+  }
+];
